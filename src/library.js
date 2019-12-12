c40323a3e61af384b775c3837439363a80c22d8b
Daniel Llatas Spiers <daniel.llatas.spiers@volumental.com>
2019-12-12 17:25:56 +0100

    A place to put nice synths

---
+++ src/library.js
@@ -0,0 +1,16 @@
+const pwm = {
+          "oscillator" : {
+                  "type" : "pwm",
+                  "modulationFrequency" : 0.2
+          },
+          "envelope" : {
+                  "attack" : 0.02,
+                  "decay" : 0.1,
+                  "sustain" : 0.2,
+                  "release" : 0.9,
+          }
+  }
+
+export default {
+	pwm
+}
