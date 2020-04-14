c86ecde1199a580b58c3b4505a5acda92e5ce45d
Daniel Llatas Spiers <danllatas@gmail.com>
2020-04-14 21:33:46 +0200

    Updates

---
+++ precache-manifest.5ef5be198f73de3bad9843b1b807be37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c660586fb3d33d698e26169506d36fec",
+    "url": "/synesthesizer/index.html"
+  },
+  {
+    "revision": "971686ef37a7420185bf",
+    "url": "/synesthesizer/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5c3bc9df0103c06d7b7b",
+    "url": "/synesthesizer/static/js/2.d15ceae4.chunk.js"
+  },
+  {
+    "revision": "a4238d9b9f29bfeea1cc4e1b5f7ba331",
+    "url": "/synesthesizer/static/js/2.d15ceae4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "971686ef37a7420185bf",
+    "url": "/synesthesizer/static/js/main.9f724459.chunk.js"
+  },
+  {
+    "revision": "99e218c19d9059da5851",
+    "url": "/synesthesizer/static/js/runtime-main.2c9a8652.js"
+  }
+]);
